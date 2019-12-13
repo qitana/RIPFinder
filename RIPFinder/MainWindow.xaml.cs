2a9fd1c27f220fb90f16dfbd058e21215120fa23
qitana <53415856+qitana@users.noreply.github.com>
2019-12-13 17:35:50 +0900

    update

--- RIPFinder/MainWindow.xaml.cs
+++ RIPFinder/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
@@ -46,8 +47,9 @@ namespace RIPFinder
             Button_SelectProcess.IsEnabled = false;
             Button_StartScan.IsEnabled = false;
             TextBox_FilterString.IsEnabled = false;
-            CheckBox_ExcludeZero.IsEnabled = false;
             CheckBox_AllModules.IsEnabled = false;
+            Button_SaveDump.IsEnabled = false;
+            Button_ExportResults.IsEnabled = false;
 
             List<string> filters = new List<string>();
             if (!string.IsNullOrWhiteSpace(TextBox_FilterString.Text))
@@ -65,23 +67,22 @@ namespace RIPFinder
                 }
             }
 
-            foreach(var f in filters)
+            foreach (var f in filters)
             {
                 TextBox_Log.AppendText($"filter: {f}" + "\r\n");
             }
 
-            bool excludeZero = CheckBox_ExcludeZero.IsChecked ?? false;
             bool searchFromAllModules = CheckBox_AllModules.IsChecked ?? false;
 
             List<ProcessModule> ProcessModules = new List<ProcessModule>();
 
-            if(searchFromAllModules)
+            if (searchFromAllModules)
             {
-                foreach(ProcessModule m in TargetProcess.Modules)
+                foreach (ProcessModule m in TargetProcess.Modules)
                 {
                     ProcessModules.Add(m);
                 }
-                
+
             }
             else
             {
@@ -104,8 +105,8 @@ namespace RIPFinder
                         TextBox_Log.AppendText($"Module Name: {m.ModuleName}" + "\r\n");
                         TextBox_Log.AppendText($"File Name: {m.FileName}" + "\r\n");
                         TextBox_Log.AppendText($"Module Size: {moduleMemorySize.ToString("#,0")} Byte" + "\r\n");
-                        TextBox_Log.AppendText($"Start Address: {startAddres.ToInt64()}" + "\r\n");
-                        TextBox_Log.AppendText($"End Address  : {endAddres.ToInt64()}" + "\r\n");
+                        TextBox_Log.AppendText($"Start Address: {startAddres.ToInt64().ToString("X2")} + ({startAddres.ToInt64()})" + "\r\n");
+                        TextBox_Log.AppendText($"End Address  : {endAddres.ToInt64().ToString("X2")} + ({endAddres.ToInt64()})" + "\r\n");
                         TextBox_Log.AppendText($"Scan started. Please wait..." + "  ");
 
                     }));
@@ -139,26 +140,20 @@ namespace RIPFinder
                                 entry.AddressValueInt64 = BitConverter.ToInt32(buffer, i);
                                 entry.TargetAddress = new IntPtr(entry.Address.ToInt64() + entry.AddressValueInt64 + 4);
 
+
                                 if (entry.TargetAddress.ToInt64() < startAddres.ToInt64() || entry.TargetAddress.ToInt64() > endAddres.ToInt64())
                                 {
                                     continue;
                                 }
 
-                                entry.TargetAddressValue = Helper.GetByteArray(TargetProcess, entry.TargetAddress, 8);
+                                entry.AddressRelativeString = m.ModuleName + "+0x" + (entry.Address.ToInt64() - startAddres.ToInt64()).ToString("X2");
+                                entry.TargetAddressRelativeString = m.ModuleName + "+0x" + (entry.TargetAddress.ToInt64() - startAddres.ToInt64()).ToString("X2");
 
-                                if (excludeZero)
-                                {
-                                    if (entry.TargetAddressValueInt64 == 0)
-                                    {
-                                        continue;
-                                    }
-                                }
+                                entry.TargetAddressValue = Helper.GetByteArray(TargetProcess, entry.TargetAddress, 8);
 
                                 if (filters.Any() &&
                                 !filters.Any(x => x == entry.TargetAddressString) &&
-                                !filters.Any(x => x == entry.TargetAddressValueHexString) &&
-                                !filters.Any(x => x == entry.TargetAddressValueInt64.ToString()) &&
-                                !filters.Any(x => x == entry.TargetAddressValueUInt64.ToString()))
+                                !filters.Any(x => x == entry.TargetAddressValueHexString))
                                 {
                                     continue;
                                 }
@@ -196,14 +191,15 @@ namespace RIPFinder
             Button_SelectProcess.IsEnabled = true;
             Button_StartScan.IsEnabled = true;
             TextBox_FilterString.IsEnabled = true;
-            CheckBox_ExcludeZero.IsEnabled = true;
             CheckBox_AllModules.IsEnabled = true;
+            Button_SaveDump.IsEnabled = true;
+            Button_ExportResults.IsEnabled = true;
         }
 
         private void DataGrid_RIP_CopyAddress(object sender, RoutedEventArgs e)
         {
             RIPEntry entry = this.DataGrid_RIP.SelectedItem as RIPEntry;
-            if(entry != null)
+            if (entry != null)
             {
                 try
                 {
@@ -211,6 +207,129 @@ namespace RIPFinder
                 }
                 catch
                 { }
+            }
+        }
+
+        private async void Button_SaveDump_Click(object sender, RoutedEventArgs e)
+        {
+            if (TargetProcess == null) { return; }
+
+            var dialog = new SaveFileDialog();
+            dialog.Filter = "binary file (*.bin)|*.bin|all files (*.*)|*.*";
+            if (dialog.ShowDialog() == true)
+            {
+                Button_SelectProcess.IsEnabled = false;
+                Button_StartScan.IsEnabled = false;
+                TextBox_FilterString.IsEnabled = false;
+                CheckBox_AllModules.IsEnabled = false;
+                Button_SaveDump.IsEnabled = false;
+                Button_ExportResults.IsEnabled = false;
+
+                var task = Task.Run(() =>
+                {
+
+                    var binFile = dialog.FileName;
+                    System.IO.FileStream binFs = new System.IO.FileStream(binFile, System.IO.FileMode.Create, System.IO.FileAccess.Write);
+                    System.IO.StreamWriter txt128Sr = new System.IO.StreamWriter(binFile + ".128.txt", false, System.Text.Encoding.ASCII);
+
+                    int moduleMemorySize = TargetProcess.MainModule.ModuleMemorySize;
+                    IntPtr startAddres = TargetProcess.MainModule.BaseAddress;
+                    IntPtr endAddres = new IntPtr(TargetProcess.MainModule.BaseAddress.ToInt64() + moduleMemorySize);
+
+                    IntPtr currentAddress = startAddres;
+                    int bufferSize = 1 * 1024 * 1024;
+                    byte[] buffer = new byte[bufferSize];
+
+
+                    while (currentAddress.ToInt64() < endAddres.ToInt64())
+                    {
+                        // size
+                        IntPtr nSize = new IntPtr(bufferSize);
+
+                        // if remaining memory size is less than splitSize, change nSize to remaining size
+                        if (IntPtr.Add(currentAddress, bufferSize).ToInt64() > endAddres.ToInt64())
+                        {
+                            nSize = (IntPtr)(endAddres.ToInt64() - currentAddress.ToInt64());
+                        }
+
+                        IntPtr numberOfBytesRead = IntPtr.Zero;
+                        if (Helper.ReadProcessMemory(TargetProcess.Handle, currentAddress, buffer, nSize, ref numberOfBytesRead))
+                        {
+                            binFs.Write(buffer, 0, numberOfBytesRead.ToInt32());
+
+                            byte[] b = new byte[numberOfBytesRead.ToInt32()];
+                            Array.Copy(buffer, 0, b, 0, numberOfBytesRead.ToInt32());
+                            var t = System.Text.RegularExpressions.Regex.Replace(BitConverter.ToString(b).Replace("-", ""), @"(?<=\G.{128})(?!$)", Environment.NewLine);
+                            txt128Sr.Write(t);
+
+
+                        }
+
+
+                        currentAddress = new IntPtr(currentAddress.ToInt64() + numberOfBytesRead.ToInt64());
+                    }
+                    binFs.Close();
+                    txt128Sr.Close();
+                });
+
+                await task;
+
+                MessageBox.Show("Complete.");
+
+                Button_SelectProcess.IsEnabled = true;
+                Button_StartScan.IsEnabled = true;
+                TextBox_FilterString.IsEnabled = true;
+                CheckBox_AllModules.IsEnabled = true;
+                Button_SaveDump.IsEnabled = true;
+                Button_ExportResults.IsEnabled = true;
+
+            }
+        }
+
+        private async void Button_ExportResults_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.DataGrid_RIP == null) { return; }
+            if (this.DataGrid_RIP.Items == null) { return; }
+
+            var dialog = new SaveFileDialog();
+            dialog.Filter = "txt file (*.txt)|*.txt|all files (*.*)|*.*";
+            if (dialog.ShowDialog() == true)
+            {
+                Button_SelectProcess.IsEnabled = false;
+                Button_StartScan.IsEnabled = false;
+                TextBox_FilterString.IsEnabled = false;
+                CheckBox_AllModules.IsEnabled = false;
+                Button_SaveDump.IsEnabled = false;
+                Button_ExportResults.IsEnabled = false;
+
+                var task = Task.Run(() =>
+                {
+                    var file = dialog.FileName;
+                    System.IO.StreamWriter sr = new System.IO.StreamWriter(file, false, System.Text.Encoding.ASCII);
+
+                    foreach (var item in this.DataGrid_RIP.Items)
+                    {
+                        RIPEntry entry = item as RIPEntry;
+                        string csv = "";
+                        csv += String.Format("{0, -30}", entry.AddressRelativeString) + " ";
+                        csv += String.Format("{0, -20}", entry.AddressString) + " ";
+                        csv += String.Format("{0, -30}", entry.TargetAddressRelativeString) + " ";
+                        csv += String.Format("{0, -20}", entry.TargetAddressString) + " ";
+                        csv += String.Format("{0, 20}", entry.TargetAddressValueHexString) + Environment.NewLine;
+                        sr.Write(csv);
+                    }
+
+                    sr.Close();
+                });
+
+                await task;
+                MessageBox.Show("Complete.");
+                Button_SelectProcess.IsEnabled = true;
+                Button_StartScan.IsEnabled = true;
+                TextBox_FilterString.IsEnabled = true;
+                CheckBox_AllModules.IsEnabled = true;
+                Button_SaveDump.IsEnabled = true;
+                Button_ExportResults.IsEnabled = true;
             }
         }
     }
@@ -224,32 +343,13 @@ namespace RIPFinder
     {
         public IntPtr Address { get; set; } = IntPtr.Zero;
         public string AddressString => Address.ToInt64().ToString("X2");
+        public string AddressRelativeString { get; set; }
         public Int64 AddressValueInt64 { get; set; } = 0;
         public IntPtr TargetAddress { get; set; } = IntPtr.Zero;
-
         public string TargetAddressString => TargetAddress.ToInt64().ToString("X2");
+        public string TargetAddressRelativeString { get; set; }
         public byte[] TargetAddressValue { get; set; } = null;
         public string TargetAddressValueHexString => BitConverter.ToString(TargetAddressValue).Replace("-", "");
-        public Int64 TargetAddressValueInt64
-        {
-            get
-            {
-                byte[] newArray = new byte[8];
-                Array.Copy(TargetAddressValue, newArray, 8);
-                Array.Reverse(newArray);
-                return BitConverter.ToInt64(newArray, 0);
-            }
-        }
-        public UInt64 TargetAddressValueUInt64
-        {
-            get
-            {
-                byte[] newArray = new byte[8];
-                Array.Copy(TargetAddressValue, newArray, 8);
-                Array.Reverse(newArray);
-                return BitConverter.ToUInt64(newArray, 0);
-            }
-        }
 
     }
 }
